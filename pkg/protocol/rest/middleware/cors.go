206ae3c4ace6420fb15b3faf4ef3695027ff7dfb
niels <niels@appademic.nl>
2019-02-22 01:00:54 +0100

    Added support for OPTIONS request

--- pkg/protocol/rest/middleware/cors.go
+++ pkg/protocol/rest/middleware/cors.go
@@ -1,15 +1,22 @@
 package middleware
 
 import (
-  "net/http"
+	"net/http"
 )
 
 func AddCors(h http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Access-Control-Allow-Origin", "*")
-    w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-    h.ServeHTTP(w, r)
-  })
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+		if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+			w.WriteHeader(200)
+			w.Write([]byte(""))
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
