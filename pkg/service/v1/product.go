6c9d9e7fb7a7751b2d865ae06fadac0eccaf6aa9
Niels <nielsla95@gmail.com>
2019-05-01 23:36:01 +0200

    feat(product): list product

--- pkg/service/v1/product.go
+++ pkg/service/v1/product.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"context"
 	"fmt"
+	"strconv"
 
 	v1 "github.com/GameComponent/economy-service/pkg/api/v1"
 )
@@ -24,6 +25,51 @@ func (s *economyServiceServer) CreateProduct(ctx context.Context, req *v1.Create
 	return &v1.CreateProductResponse{
 		Api:     apiVersion,
 		Product: product,
+	}, nil
+}
+
+func (s *economyServiceServer) ListProduct(ctx context.Context, req *v1.ListProductRequest) (*v1.ListProductResponse, error) {
+	fmt.Println("ListProduct")
+
+	// check if the API version requested by client is supported by server
+	if err := s.checkAPI(req.Api); err != nil {
+		return nil, err
+	}
+
+	// Parse the page token
+	var parsedToken int64
+	parsedToken, _ = strconv.ParseInt(req.GetPageToken(), 10, 32)
+
+	// Get the limit
+	limit := req.GetPageSize()
+	if limit == 0 {
+		limit = 100
+	}
+
+	// Get the offset
+	offset := int32(0)
+	if len(req.GetPageToken()) > 0 {
+		offset = int32(parsedToken) * limit
+	}
+
+	// Get the products
+	products, totalSize, err := s.productRepository.List(ctx, limit, offset)
+	if err != nil {
+		return nil, err
+	}
+
+	// Determine if there is a next page
+	var nextPageToken string
+	if totalSize > (offset + limit) {
+		nextPage := int32(parsedToken) + 1
+		nextPageToken = strconv.Itoa(int(nextPage))
+	}
+
+	return &v1.ListProductResponse{
+		Api:           apiVersion,
+		Products:      products,
+		TotalSize:     totalSize,
+		NextPageToken: nextPageToken,
 	}, nil
 }
 
