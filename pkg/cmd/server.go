dbe588cab4d59ded6c37335d5eb58f4a9a4cb127
Niels <nielsla95@gmail.com>
2019-07-24 23:51:22 +0200

    feat(server): added Viper for YAML, flag and ENV configuration

--- pkg/cmd/server.go
+++ pkg/cmd/server.go
@@ -20,39 +20,85 @@ import (
 	shoprepository "github.com/GameComponent/economy-service/pkg/repository/shop"
 	storagerepository "github.com/GameComponent/economy-service/pkg/repository/storage"
 	v1 "github.com/GameComponent/economy-service/pkg/service/v1"
+	pflag "github.com/spf13/pflag"
+	viper "github.com/spf13/viper"
 )
 
 // Config for the server
 type Config struct {
-	GRPCPort         string
-	HTTPPort         string
-	DatabaseHost     string
-	DatabasePort     string
-	DatabaseUser     string
-	DatabasePassword string
-	DatabaseName     string
-	DatabaseSsl      string
-	LogLevel         int
-	LogTimeFormat    string
+	GRPCPort         string `mapstructure:"grpc_port"`
+	HTTPPort         string `mapstructure:"http_port"`
+	DatabaseHost     string `mapstructure:"db_host"`
+	DatabasePort     string `mapstructure:"db_port"`
+	DatabaseUser     string `mapstructure:"db_user"`
+	DatabasePassword string `mapstructure:"db_password"`
+	DatabaseName     string `mapstructure:"db_name"`
+	DatabaseSsl      string `mapstructure:"db_ssl"`
+	LogLevel         int    `mapstructure:"log_level"`
+	LogTimeFormat    string `mapstructure:"log_time_format"`
 }
 
 // RunServer runs gRPC server and HTTP gateway
 func RunServer() error {
 	ctx := context.Background()
 
-	// Get configuration
+	// Set the configuration
+	v := viper.New()
+
+	// Load from environment variables
+	v.AutomaticEnv()
+
+	// Set the defaults
+	v.SetDefault("grpc_port", "3000")
+	v.SetDefault("http_port", "8080")
+	v.SetDefault("db_host", "127.0.0.1")
+	v.SetDefault("db_port", "26257")
+	v.SetDefault("db_user", "root")
+	v.SetDefault("db_password", "")
+	v.SetDefault("db_name", "economy")
+	v.SetDefault("db_ssl", "disable")
+	v.SetDefault("log_level", "0")
+	v.SetDefault("log_time_format", "")
+
+	// Set potential config locations
+	v.SetConfigName("config")
+	v.AddConfigPath("/etc/economy-service/")
+	v.AddConfigPath("$HOME/.economy-service")
+	v.AddConfigPath(".")
+
+	// Load from config files
+	err := v.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			fmt.Println("No config file found")
+		} else {
+			return fmt.Errorf("Unable to read in config")
+		}
+	}
+
+	// Parse the flags
+	flag.String("grpc_port", "3000", "gRPC port to bind")
+	flag.String("http_port", "8080", "http port to bind")
+	flag.String("db_host", "127.0.0.1", "host of the database")
+	flag.String("db_port", "26257", "port of the database")
+	flag.String("db_user", "root", "user of the database")
+	flag.String("db_password", "", "password of the database")
+	flag.String("db_name", "economy", "name of the database")
+	flag.String("db_ssl", "disable", "ssl settings of the database")
+	flag.Int("log_level", 0, "level of the logger")
+	flag.String("log_time_format", "", "time format of the logger")
+
+	// Add flags to Viper
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+	v.BindPFlags(pflag.CommandLine)
+
+	// Unmarhsal the viper config to a struct
 	var cfg Config
-	flag.StringVar(&cfg.GRPCPort, "grpc_port", "3000", "gRPC port to bind")
-	flag.StringVar(&cfg.HTTPPort, "http_port", "8080", "http port to bind")
-	flag.StringVar(&cfg.DatabaseHost, "db_host", "127.0.0.1", "host of the database")
-	flag.StringVar(&cfg.DatabasePort, "db_port", "26257", "port of the database")
-	flag.StringVar(&cfg.DatabaseUser, "db_user", "root", "user of the database")
-	flag.StringVar(&cfg.DatabasePassword, "db_password", "", "password of the database")
-	flag.StringVar(&cfg.DatabaseName, "db_name", "economy", "name of the database")
-	flag.StringVar(&cfg.DatabaseSsl, "db_ssl", "disable", "ssl settings of the database")
-	flag.IntVar(&cfg.LogLevel, "log_level", 0, "level of the logger")
-	flag.StringVar(&cfg.LogTimeFormat, "log_time_format", "", "time format of the logger")
-	flag.Parse()
+	err = v.Unmarshal(&cfg)
+	if err != nil {
+		return fmt.Errorf("Unable to unmarshal config")
+	}
 
 	if len(cfg.GRPCPort) == 0 {
 		return fmt.Errorf("invalid TCP port for gRPC server: '%s'", cfg.GRPCPort)
@@ -64,7 +110,7 @@ func RunServer() error {
 	}
 
 	// Setup database & migrate
-	_, err := database.Init(
+	_, err = database.Init(
 		cfg.DatabaseHost,
 		cfg.DatabasePort,
 		cfg.DatabaseUser,
